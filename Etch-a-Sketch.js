a0672e56cc7f90f5f48a31b7f4dd799e8dab521d
Jonathan Branch <155466835+jonbranch99@users.noreply.github.com>
2024-04-03 17:12:54 -0400

    add random color
    grid now generates random colors when you mouse over

--- Etch-a-Sketch.js
+++ Etch-a-Sketch.js
@@ -20,7 +20,8 @@ function makeColumns(cellNum) {
 }
 
 function changeColor(event) {
-  event.target.style.backgroundColor = "black";
+  const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  event.target.style.backgroundColor = randomColor;
 }
 
 function defaultGrid() {
